bd90c7af0fc802da8862df15f477f7c24f6ca3c3
Gustavo Gilberto Godinho <11860941+gustavogodinho@users.noreply.github.com>
2019-04-17 23:14:41 -0300

    refatoração

--- Preposicao.cs
+++ Preposicao.cs
@@ -1,75 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Klingon
 {
     class Preposicao
     {
-
-        static void Main(string[] args)
+        private static void Main()
         {
-
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Work\Financas360\textoB.txt");
-
+            string[] Lines = System.IO.File.ReadAllLines(@"C:\Work\Financas360\textoB.txt");
             string[] Words;
             int count = 0;
 
-            foreach (string line in lines)
+            foreach (string line in Lines)
             {
                 Words = line.Split(' ');
 
                 foreach (string word in Words)
                 {
-                    //preposição
                     if (word.Length == 3 && (!word.Contains("d")))
                     {
-
                         if (LetraBar(word))
                         {
                             count = count + 1;
                         }
                     }
                 }
-
             }
             Console.WriteLine("Qtd Preposições: " + count);
             Console.ReadKey();
-
-
         }
-
         private static bool LetraBar(string palavra)
         {
             bool retorno = false;
-            List<string> li = new List<string>
-            {
-                "b",
-                "r",
-                "q",
-               // "d",
-                "n",
-                "x",
-                "j",
-                "m",
-                "v",
-                "h",
-                "t",
-                "c",
-                "g",
-                "z",
-                "p"
-            };
+            string letrasBar = "brqdnxjmvhtcgzp";
 
-            for (int i = 0; i < li.Count; i++)
+            for (int i = 0; i < letrasBar.Length; i++)
             {
-                if (palavra.Substring(palavra.Length - 1, 1) == li[i])
+                if (palavra.Substring(palavra.Length - 1, 1) == letrasBar[i].ToString())
                 {
                     retorno = true;
                     break;
-                }
-                else
-                {
-                  
                 }
             }
             return retorno;
