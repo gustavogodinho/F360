75e7845991011c0eb3216890a4f0ad3a8a79f706
Gustavo Gilberto Godinho <11860941+gustavogodinho@users.noreply.github.com>
2019-04-17 23:15:33 -0300

    refatoração

--- NumeroBonito.cs
+++ NumeroBonito.cs
@@ -6,16 +6,15 @@ namespace KlingonNumero
 {
     class NumeroBonito
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-
             string[] lines = System.IO.File.ReadAllLines(@"C:\Work\Financas360\textoB.txt");
             string[] linesNotDup = lines.Distinct().ToArray();
 
-            GetNumber(linesNotDup);
+            ObterNumero(linesNotDup);
         }
 
-        private static void GetNumber(string[] linesNotDup)
+        private static void ObterNumero(string[] linesNotDup)
         {
             string[] Words;
             int countNumeroBonito = 0;
@@ -34,48 +33,21 @@ namespace KlingonNumero
                         letra = line2.Substring(i, 1);
                         numero += Math.Pow(20, i) * PesoLetra(letra);
                     }
-                    ///-é maior ou igual a 440566 - é divisível por 3
                     if (numero >= 440566 && ((numero % 3) == 0))
                     {
                         countNumeroBonito = (countNumeroBonito + 1);
                     }
                 }
-
                 Console.WriteLine("Números bonitos distintos: " + countNumeroBonito);
                 Console.ReadKey();
             }
         }
-
         private static int PesoLetra(string letra)
         {
-            List<string> li = new List<string>
-            {
-                "k",
-                "b",
-                "w",
-                "r",
-                "q",
-                "d",
-                "n",
-                "f",
-                "x",
-                "x",
-                "j",
-                "m",
-                "l",
-                "v",
-                "h",
-                "t",
-                "c",
-                "g",
-                "z",
-                "p",
-                "s"
-            };
+            string alfabetoKingon = "kbwrqdnfxxjmlvhtcgzps";
+            int valorLetra = alfabetoKingon.IndexOf(letra);
 
-            int ValorLetra = li.IndexOf(letra);
-
-            return (ValorLetra - 1);
+            return (valorLetra - 1);
         }
     }
 }
